2b73e77bf8b9fdf94184139988cf475e536585c2
kostkaro <kostka.ro@gmail.com>
2022-11-13 20:48:29 +0100

    add modal

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
   }
